4b567be73715d4a5a13b0989f2f6fdff8023b832
Trevor Obermann <tlo@ocsnet.com>
2017-04-24 17:32:17 -0400

    Initial setup file commit

---
+++ data_analysis/twitter_data_access.py
@@ -0,0 +1,46 @@
+import json
+from tweepy import OAuthHandler, Stream, API
+from tweepy import StreamListener
+
+consumer_key = '8zhY3gkSuNxnUvQVlLxxYYwGv'
+consumer_secret = 'IcnG9YQY6hGYp2q9cWKW6Dfp7EDFKJWMnekSjNGAWsXPy2hr4u'
+access_token = '84374247-7fIBIm0wzJa5e3TMenjkdtIVo5TFsnOLvl6yQdqPm'
+access_token_secret = 'PHcOTjc0L46MfnYhE6uQQlleBW90VdzMoQvczycaIx9jP'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+
+class PrintListener(StreamListener):
+    def on_status(self, status):
+        if not status.text[:3] == 'RT ':
+            print(status.text)
+            print(status.author.screen_name, status.created_at, status.source, '\n')
+
+    def on_error(self, status_code):
+        print('Error Code: {}'.format(status_code))
+        return True  # Keep stream alive
+
+    def on_timeout(self):
+        print('Listener timed out!')
+        return True  # Keep stream alive
+
+
+def print_to_terminal():
+    listener = PrintListener()
+    stream = Stream(auth, listener)
+    languages = ('en',)
+    stream.sample(languages=languages)
+
+
+def pull_down_tweets(screen_name):
+    api = API(auth)
+    tweets = api.user_timeline(screen_name=screen_name, count=200)
+    for tweet in tweets:
+        print(json.dumps(tweet._json, indent=4))
+
+
+if __name__ == '__main__':
+    #  print_to_terminal()
+    pull_down_tweets(auth.username)
+   
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_analysis',
+    version='0.0.1',
+    url='www.github.com/Trax/data_analysis',
+    license='BSD',
+    author='Trevor Obermann',
+    packages=find_packages(),
+    install_requires=['PyQt5', 'pandas', 'sqlalchemy', 'nltk', 'numpy', 'jupyter', 'python-twitter'],
+    entry_points={},
+    extras_require={'dev': ['flake8', ]},
+)
